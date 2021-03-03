23fe2d167d9608c2c3a64b4d7dbc506000ed4aa2
Filippo Varini <filippovarini@Filippos-MacBook-Pro.local>
2021-03-03 18:18:09 +0100

    basic challenger routes

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
-
+const challengerRouter = require("./routes/Challenger");
 const pool = require("./db");
 
 const app = express();
 const port = 5000;
 
+// middlewares
 app.use(express.json());
+
+//routers
+app.use("/challenger", challengerRouter);
 
 app.get("/", (_, res) => {
   res.send("<h1>Hello World</h1>");
@@ -19,6 +23,10 @@ app.get("/see", async (req, res) => {
       console.log(e.message);
       res.send("shit");
     });
+});
+
+app.use("*", (_, res) => {
+  res.status(404).send("404 - nothing found");
 });
 
 app.listen(port, () => {
